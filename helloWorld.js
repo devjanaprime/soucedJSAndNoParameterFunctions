5c67ae3cd0c5b52959ca313b636e87a5d7e3a1c9
devjanaprime <dev@primeacademy.io>
2016-07-13 19:21:11 -0500

    first

---
+++ helloWorld.js
@@ -0,0 +1,9 @@
+var sourcedGreeting = "Hello World from sourced js";
+console.log( "sourcedGreeting: " + sourcedGreeting );
+
+var helloWorldFunction = function(){
+  var sourcedGreetingFromFunction = "Hello World from sourced js";
+  console.log( "sourcedGreetingFromFunction: " + sourcedGreetingFromFunction );
+};
+
+helloWorldFunction();
